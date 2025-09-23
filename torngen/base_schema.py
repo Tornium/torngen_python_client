c595315fb49289492f9b9988d9ff5e9735575ab7
tiksan <webmaster@deek.sh>
2025-09-23 14:48:31 -0500

    Updated literal match error on parsing
    Signed-off-by: tiksan <webmaster@deek.sh>

--- torngen/base_schema.py
+++ torngen/base_schema.py
@@ -90,7 +90,7 @@ class BaseSchema(ABC):
         elif origin is typing.Literal:
             args_string = textwrap.shorten(", ".join(args), width=30, placeholder="...")
             raise ValueError(
-                f"Data does not match any value in the literal with values {args_string}"
+                f"Data {data} does not match any value in the literal with values {args_string}"
             )
 
         if origin in (list, typing.List):
