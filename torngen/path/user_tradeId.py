62da12ff5d5f1a208cd8d90ccd2ebcab15033b5b
dssecret <webmaster@deek.sh>
2026-08-08 00:34:56 +0000

    [torngen] Generated v6.6.1

--- torngen/path/user_tradeId.py
+++ torngen/path/user_tradeId.py
@@ -23,7 +23,7 @@ class UserTradeId(BaseQuery):
 
     # Parameters
     - tradeId : Trade id
-    - timestamp : Timestamp to bypass cache or get the data in specific point in time
+    - timestamp : Timestamp to bypass cache
     - comment : Comment for your tool/service/bot/website to be visible in the logs.
     - key : API key (Limited). It&#39;s not required to use this parameter when passing the API key via the Authorization header.
     
